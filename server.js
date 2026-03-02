2a692da6fd9fea402515b195c1fadd78f2c52b98
LEONARDO PEREIRA DA SILVA <leonardo_p_da-silva@estudante.sesisenai.org.br>
2026-03-02 11:51:21 -0300

    PA

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 import Fastify from 'fastify';
+import { Pool } from 'pg';
+
+const sql = new Pool({
+    user: "postgres",
+    password: "senai",
+    host: "localhost",
+    port: 5432,
+    database: "receitas"
+})
+
 
 const servidor = Fastify()
 
-servidor.get('/usuarios', () => {
-    return "Esta funcionando"
+servidor.get('/usuarios', async () => {
+   const resultado = await sql.query("SELECT * FROM usuario")
+    return resultado.rows
 })
 
+servidor.post("/usuarios", async (request, reply) => {
+    const nome = request.body.nome;
+    const senha = request.body.senha;
+    const resultado = await sql.query('INSERT INTO usuario (nome, senha) VALUES ($1, $2)', [nome, senha])
+    return 'usuario criado com sucesso'
+})
 servidor.listen({port: 3000})
  
