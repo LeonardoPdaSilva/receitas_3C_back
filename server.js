fbb3d5ea3dd226424b106c0985d4b784e1d5691b
LEONARDO PEREIRA DA SILVA <leonardo_p_da-silva@estudante.sesisenai.org.br>
2026-03-16 11:23:35 -0300

    primeiro commit

--- server.js
+++ server.js
@@ -20,8 +20,27 @@ servidor.get('/usuarios', async () => {
 servidor.post("/usuarios", async (request, reply) => {
     const nome = request.body.nome;
     const senha = request.body.senha;
-    const resultado = await sql.query('INSERT INTO usuario (nome, senha) VALUES ($1, $2)', [nome, senha])
+    const email = request.body.email;
+    const resultado = await sql.query('INSERT INTO usuario (nome, senha, email) VALUES ($1, $2, $3)', [nome, senha, email])
     return 'usuario criado com sucesso'
 })
+
+servidor.post("/login", async (request, reply) => {
+    const email = request.body.email
+    const senha = request.body.senha
+    const resultado = await sql.query('SELECT * FROM USUARIO WHERE email = $1 AND senha = $2',
+    [email, senha])
+    if(resultado.rows.length === 0){
+        return reply.status(401).send({
+            mensagem: "email ou senha invalido"
+        })   
+    } else{
+        return reply.status(200).send({
+            mensagem: "Login realizado com sucesso",
+            usuario: resultado.rows[0]
+        })
+    }
+})
+
 servidor.listen({port: 3000})
  
